4162b4adfb5003c5a7e21ebb444cdb6170659e3c
Deepanshu Goel <anshu@mages.edu.sg>
2024-08-28 19:04:24 +0530

    added: cors

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,17 @@ import {
 import { createRecords, updateRecords } from "./controllers/create-records";
 import mongoose from "mongoose";
 import { createLeague } from "./controllers/league";
-
+import cors from "cors";
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 mongoose
   .connect(process.env.MONGO_URI || "mongodb://localhost:27017/aptos-db", {
     autoCreate: true,
